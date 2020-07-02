a6be41e0078100cf437ed0c4db99bd8f93f6c938
Phil Sturgeon <me@philsturgeon.uk>
2020-07-02 15:15:21 +0200

    remove google analytics and compress images

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,24 +89,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-XXXX-Y', // TODO: Phil add ID
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-disqus',
       options: {
         shortname: 'philsturgeon',
